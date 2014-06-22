ae6f54dc47fd2ca49d5e25587c560337a078c8f1
Ryan Mullins <rsm5068@gmail.com>
2014-06-22 19:54:47 -0400

    Added universal escape route for participants

--- Sympol/Sympol/SYMPOLAppDelegate.m
+++ Sympol/Sympol/SYMPOLAppDelegate.m
@@ -8,11 +8,11 @@
 
 #import "SYMPOLAppDelegate.h"
 #import "SYMPOLEvaluationController.h"
+#import "SYMPOLEvaluationModel.h"
 
 @implementation SYMPOLAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL) application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     UIViewController * rootViewController = [[UIStoryboard storyboardWithName:@"Evaluation" bundle:nil] instantiateInitialViewController];
@@ -35,10 +35,8 @@
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    [[SYMPOLEvaluationModel sharedEvaluationModel] reset];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
